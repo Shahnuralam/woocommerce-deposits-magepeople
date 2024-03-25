14069e7ca628a1e615ed68e7443f0e4a88458bbb
ShahnurAlam <shahnurlfe@gmail.com>
2024-03-25 14:57:10 +0600

    Initial commit

---
+++ templates/single-product/wc-deposits-product-single-plan.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * The Template for displaying single payment plan display on single product page
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/wc-deposits-product-single-plan.php.
+ *
+ * @package MagePeople\WCDP\Templates
+ * @version 3.0.0
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+?>
+<li>
+    <input data-id="<?php echo $plan_id; ?>" <?php echo $count === 0 ? 'checked' : ''; ?>
+           type="radio" class="option-input" value="<?php echo $plan_id; ?>"
+           name="<?php echo $product->get_id(); ?>-selected-plan"/>
+    <label><?php echo $payment_plan['name']; ?></label>
+
+
+    <span> <a data-expanded="no"
+              data-view-text="<?php esc_html_e('View details', 'Advanced Partial Payment and Deposit For Woocommerce'); ?>"
+              data-hide-text="<?php esc_html_e('Hide details', 'Advanced Partial Payment and Deposit For Woocommerce'); ?>"
+              data-id="<?php echo $plan_id; ?>"
+              class="wcdp-view-plan-details"><?php esc_html_e('View details', 'Advanced Partial Payment and Deposit For Woocommerce'); ?></a></span>
+
+    <div style="display:none" class="wcdp-single-plan plan-details-<?php echo $plan_id; ?>"
+    >
+
+
+        <div>
+            <p><?php esc_html_e($payment_plan['description'], 'Advanced Partial Payment and Deposit For Woocommerce'); ?></p>
+        </div>
+
+        <?php if ($product->get_type() !== 'grouped') { ?>
+
+
+            <div>
+                <p><strong><?php esc_html_e('Payments Total', 'Advanced Partial Payment and Deposit For Woocommerce'); ?>
+                        : <?php echo wc_price($payment_plan['plan_total']); ?></strong></p>
+            </div>
+
+            <div>
+                <p><?php esc_html_e($deposit_text, 'Advanced Partial Payment and Deposit For Woocommerce'); ?>
+                    : <?php echo wc_price($payment_plan['deposit_amount']); ?></p>
+            </div>
+
+            <table>
+                <thead>
+                <th><?php esc_html_e('Payment Date', 'Advanced Partial Payment and Deposit For Woocommerce') ?></th>
+                <th><?php esc_html_e('Amount', 'Advanced Partial Payment and Deposit For Woocommerce') ?></th>
+                </thead>
+                <tbody>
+                <?php
+                $payment_timestamp = current_time('timestamp');
+                foreach ($payment_plan['details']['payment-plan'] as $plan_line) {
+
+                    if (isset($plan_line['date']) && !empty($plan_line['date'])) {
+                        $payment_timestamp = strtotime($plan_line['date']);
+                    } else {
+                        $after = $plan_line['after'];
+                        $after_term = $plan_line['after-term'];
+                        $payment_timestamp = strtotime(date('Y-m-d', $payment_timestamp) . "+{$plan_line['after']} {$plan_line['after-term']}s");
+                    }
+
+                    ?>
+                    <tr>
+                        <td><?php echo date_i18n(get_option('date_format'), $payment_timestamp) ?></td>
+                        <td><?php echo wc_price($plan_line['line_amount']); ?></td>
+                    </tr>
+                    <?php
+
+
+                }
+
+                ?>
+                </tbody>
+            </table>
+        <?php } ?>
+    </div>
+</li>
---
+++ templates/single-product/wc-deposits-product-slider.php
@@ -0,0 +1,104 @@
+<?php
+// Retrieve the value of wc_deposits_storewide_deposit_amount_type and wc_deposits_storewide_deposit_amount
+$storewide_deposit_amount_type = get_option('wc_deposits_storewide_deposit_amount_type');
+$storewide_deposit_amount = get_option('wc_deposits_storewide_deposit_amount');
+$storewide_deposit_enabled_details = get_option('wc_deposits_storewide_deposit_enabled_details');
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+do_action('wc_deposits_enqueue_product_scripts');
+if ($force_deposit === 'yes') $default_checked = 'deposit';
+$hide = get_option('wc_deposits_hide_ui_when_forced', 'no') === 'yes';
+
+// Check if storewide deposit details are enabled
+if ($storewide_deposit_enabled_details === 'yes') {
+    // Display the deposit option
+?>
+    <div data-ajax-refresh="<?php echo $ajax_refresh; ?>" data-product_id="<?php echo $product->get_id(); ?>" class='webtomizer_wcdp_single_deposit_form <?php echo $basic_buttons ? 'basic-wc-deposits-options-form' : 'wc-deposits-options-form'; ?>'>
+        
+        <?php
+        if (!$has_payment_plans && $product->get_type() !== 'grouped') { ?>
+            <label class='deposit-option'>
+                <!--<hr class='separator' />-->
+                Partial Payment Details:
+                <br>
+                Deposit:
+                <?php if ($product->get_type() === 'variable' && $deposit_info['type'] === 'percent') {
+                    ?> <span id='deposit-amount'><?php echo $deposit_amount . '%'; ?></span><?php
+                } else {
+                    ?> <span id='deposit-amount'><?php echo wc_price($deposit_amount); ?></span><?php
+                } ?>
+                <br>
+                Deposit Type:
+                <?php if ($storewide_deposit_amount_type) { ?>
+
+                    <?php if ($storewide_deposit_amount) { ?>
+                        <span id='storewide-deposit-amount'><?php echo esc_html($storewide_deposit_amount); ?></span>
+                    <?php } ?>
+                    <span id='storewide-deposit-amount-type'><?php echo esc_html($storewide_deposit_amount_type); ?></span>
+                <?php } ?>
+            </label>
+        <?php }
+        ?>
+
+        <div class="<?php echo $hide ? 'wcdp_hidden ' : '' ?>  <?php echo $basic_buttons ? 'basic-switch-Advanced Partial Payment and Deposit For Woocommerce' : 'deposit-options switch-toggle switch-candy switch-Advanced Partial Payment and Deposit For Woocommerce'; ?>">
+            <input id='<?php echo $product->get_id(); ?>-pay-deposit' class='pay-deposit input-radio' name='<?php echo $product->get_id(); ?>-deposit-radio' type='radio' <?php checked($default_checked, 'deposit'); ?> value='deposit'>
+            <label class="pay-deposit-label" for='<?php echo $product->get_id(); ?>-pay-deposit'><?php esc_html_e($deposit_text, 'Advanced Partial Payment and Deposit For Woocommerce'); ?></label>
+            <input id='<?php echo $product->get_id(); ?>-pay-full-amount' class='pay-full-amount input-radio' name='<?php echo $product->get_id(); ?>-deposit-radio' type='radio' <?php checked($default_checked, 'full'); ?>
+                <?php echo isset($force_deposit) && $force_deposit === 'yes' ? 'disabled' : '' ?> value="full">
+            <label class="pay-full-amount-label" for='<?php echo $product->get_id(); ?>-pay-full-amount'><?php esc_html_e($full_text, 'Advanced Partial Payment and Deposit For Woocommerce'); ?></label>
+            <a class='wc-deposits-switcher'></a>
+        </div>
+        <span class='deposit-message wc-deposits-notice'></span>
+        <?php
+        if ($has_payment_plans) { ?>
+            <div class="wcdp-payment-plans">
+                <fieldset>
+                    <ul>
+                        <?php
+                        $count = 0;
+                        foreach ($payment_plans as $plan_id => $payment_plan) {
+                            wc_get_template('single-product/wc-deposits-product-single-plan.php',
+                                array('count' => $count,
+                                    'plan_id' => $plan_id,
+                                    'deposit_text' => $deposit_text,
+                                    'payment_plan' => $payment_plan,
+                                    'product' => $product),
+                                '', WC_DEPOSITS_TEMPLATE_PATH);
+                            $count++;
+                        } ?>
+                    </ul>
+                </fieldset>
+            </div>
+<?php }
+    ?>
+    </div>
+<?php
+} // End if storewide deposit details are enabled
+?>
+
+
+
+
+
+<script>
+    jQuery(document).ready(function($) {
+        // Hide deposit-option initially if pay-full-amount is checked
+        if ($('#<?php echo $product->get_id(); ?>-pay-full-amount').is(':checked')) {
+            $('.deposit-option').hide();
+        }
+
+        // Toggle deposit-option visibility on radio button change
+        $('input[name="<?php echo $product->get_id(); ?>-deposit-radio"]').change(function() {
+            if ($(this).val() === 'full') {
+                $('.deposit-option').hide();
+            } else {
+                $('.deposit-option').show();
+            }
+        });
+    });
+</script>
+
+
